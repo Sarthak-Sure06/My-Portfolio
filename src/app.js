65472915a7197416ace65f83cb06726183629c78
Sarthak-Sure06 <sarthaksaxena18006@gmail.com>
2024-03-07 00:19:49 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const app = express();
+const port = process.env.PORT || 8000;
+
+const static_path = path.join(__dirname , "../public");
+const template_path = path.join(__dirname,"../templates/views");
+const partials_path = path.join(__dirname , "../templates/partials");
+
+app.set('view engine' , 'hbs');
+app.set('views' , template_path);
+hbs.registerPartials(partials_path);
+
+app.use(express.static(static_path));
+
+//routing path//
+// app.get(route,callback)
+app.get("/index",(req,res)=>{
+    res.render("index");
+})
+
+app.get("/about",(req,res)=>{
+    res.render('about');
+})
+
+app.get("/projects",(req,res)=>{
+    res.render('projects');
+})
+
+app.get("/certifications",(req,res)=>{
+    res.render('certifications');
+})
+
+app.get("/contact",(req,res)=>{
+    res.render('contact');
+})
+
+app.get("*",(req,res)=>{
+    res.send("404 error");
+})
+
+app.listen(port, ()=>{
+    console.log(8000);
+});
