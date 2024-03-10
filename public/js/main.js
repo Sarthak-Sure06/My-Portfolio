d985178687da5f96fa50726204d2432418c3afb8
Sarthak-Sure06 <sarthaksaxena18006@gmail.com>
2024-03-11 03:21:05 +0530

    complete

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Simulate loading time
+//     setTimeout(function() {
+//         // Hide preloader
+//         document.querySelector('.preloader').style.display = 'none';
+//         // Show content
+//         document.querySelector('.content').style.display = 'block';
+//     }, 1000); // Adjust time as needed
+
+//     // Remove the event listener after the first run
+//     document.removeEventListener("DOMContentLoaded", arguments.callee);
+// });
+
+
+
+  
